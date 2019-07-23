c26d18a4a75de1625c3faeb6eee3e01d6a13085f
Damilare Adedoyin <damilare182@gmail.com>
2019-07-23 21:41:50 +0100

    history api added

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const serveStatic = require("serve-static");
+var history = require("connect-history-api-fallback");
 const path = require("path");
 const app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
+var staticFileMiddleware = serveStatic(path.join(__dirname, "dist"));
+app.use(staticFileMiddleware);
+app.use(history({ verbose: true }));
+app.use(staticFileMiddleware);
+app.get("/", function(req, res) {
+  res.render(path.join(__dirname + "/"));
+});
 const port = process.env.PORT || 80;
 app.listen(port);
